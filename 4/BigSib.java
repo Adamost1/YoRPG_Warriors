0e80275d6f7e389661b892d942937686cd10eb9f
Alvin Ye <aye4@stuy.edu>
2017-11-10 08:13:51 -0500

    first commit

---
+++ 4/BigSib.java
@@ -0,0 +1,11 @@
+public class BigSib{
+    
+    public static void main(String[] args){
+	greet("Kevin");
+    }
+
+    public static void greet(String name) {
+	System.out.print("Why hello there, ");
+	System.out.println(name);
+    }
+}
